825930d7af5dd1cacd8fa55ccdf18c504a359e67
andersongames <ander.cal45@gmail.com>
2020-07-27 14:25:10 -0300

    initial
    initial setup

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Menu from './components/Menu'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
+import dadosIniciais from './data/dados_iniciais.json'
+
+function App() {
+  return (
+    <div className="App" style={{ background: "#141414" }}>
+      <Menu />
+      
+      <BannerMain
+        videoTitle = {dadosIniciais.categorias[0].videos[0].titulo}
+        url = {dadosIniciais.categorias[0].videos[0].url}
+        videoDescription = {"O que é Front-end?"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category = {dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category = {dadosIniciais.categorias[1]}
+      />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
